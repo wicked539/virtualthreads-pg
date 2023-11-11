bf8aa46df9825586c7db361ddde576abb20f0b11
Sven Müller <wicked@Svens-MacBook-Pro.local>
2023-11-11 18:01:42 +0100

    add second project

---
+++ blocking/src/main/java/de/wicked539/virtualthreadspg/blocking/BlockingApplication.java
@@ -0,0 +1,13 @@
+package de.wicked539.virtualthreadspg.blocking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlockingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlockingApplication.class, args);
+	}
+
+}
---
+++ blocking/src/test/java/de/wicked539/virtualthreadspg/blocking/BlockingApplicationTests.java
@@ -0,0 +1,13 @@
+package de.wicked539.virtualthreadspg.blocking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlockingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
